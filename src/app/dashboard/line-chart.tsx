747406768e21d5d94981e9a79e32f21790d9b2b3
Andydiii <Andydiii0813@gmail.com>
2025-07-08 13:54:26 -0400

    Learn File Colocation 1. Every folder in app is a url segment, but a page,tsx is the index file for that route. Without page.tsx, it wont show anything.
    2. the component in page.tsx has to be a export default.

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+// this wont be shown if there is no page.tsx or the page.tsx does not use this component.
+export default function LineChart() {
+  return (
+    <div>line-chart</div>
+  )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// the component in the indexpage for each folder has to have be export default
+export default function page() {
+  return (
+    <div>Dashboard</div>
+  )
+}
