5bdc1467ac92e23d1b18bf1058706fb8399917d2
Andydiii <Andydiii0813@gmail.com>
2025-08-19 18:21:55 -0400

    Learned adding navigation by Link component, also reformatted the tab to be 2 space not 4 spaces

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import { Metadata } from 'next'; // return a metadata interface
+import React from "react";
+import { Metadata } from "next"; // return a metadata interface
 
-type  Props = {
-  params: Promise<{productId: string}>
-}
+type Props = {
+  params: Promise<{ productId: string }>;
+};
 
 // dynmaic metadata
-export async function generateMetadata({params} : Props) : Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const id = (await params).productId;
   return {
     title: `Product ${id}`,
   };
-};
+}
 
-// [productId] is a dynamic file based routing, so it covers products/1 products/2 products/3 ... 
+// [productId] is a dynamic file based routing, so it covers products/1 products/2 products/3 ...
 // every page in app router receives a route parameter through prop 'params'
 // params is a promise that results to a object containing dynamic segments as key-value pair, the dynamic segment is productId in this case.
-export default async function ProductDetails( {params}: Props ) {
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return (
     // in a real app, we may want to fetch product details from an api based on product ID and render the product details instead of the hard code "ProductDetails"
-    <h1>Details of the product {productId}</h1>
-  )
+    <h1>
+        Details of the product {productId}
+    </h1>
+  );
 }
-
